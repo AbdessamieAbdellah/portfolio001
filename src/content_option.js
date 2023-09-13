3c0f1e4414575b3d7208742c74e0049afcbe3bb2
ABDESSAMIE <abdessamie.abdellah@gmail.com>
2023-09-13 00:20:05 -0700

    dataChange

--- src/content_option.js
+++ src/content_option.js
@@ -1,61 +1,72 @@
-const logotext = "ABDESSAMIE";
+const logotext = "ABDESSAMIE (Samy)";
 const meta = {
-    title: "John Doe",
+    title: "Abdessamie Abdellah",
     description: "I’m Abdessamie Abdellah react native devloper,currently working with United Airlines in USA",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Abdessamie Abdellah",
     animated: {
-        first: "I love coding",
+        first:  "I develop mobile apps for IOS & Android",
         second: "I build gorgeous UI/UX ",
-        third: "I develop mobile apps for IOS & Android",
+        third: "I love coding !" ,
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Creative React Native Developer offering 5+ years of experience providing high-impact web solutions for diverse industry world organizations. Skilled in designing, developing, and testing cross-platform mobile applications incorporating a range of technologies.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "Little Intro",
+    aboutme: "With over seven years of dedicated experience in the realm of Information Technology, I am a seasoned React JS and React Native Developer with a proven track record of delivering exceptional results. Over the course of my career, I have honed my skills to become a versatile professional, adept at crafting cross-platform mobile applications that seamlessly bridge the gap between innovation and user experience.My journey in software development has encompassed various facets of the industry, from meticulous Requirement Analysis and precise Implementation to expertly estimating project timelines. I take pride in my ability to build high-performance mobile applications that cater to both iOS and Android users, all while maintaining a keen focus on optimizing efficiency and user engagement. One of my strengths lies in the creation of reusable React components, thoughtfully designed to enhance the presentation and functionality of applications. I have a deep understanding of state and prop management, ensuring seamless data flow within my projects. Additionally, I have successfully ported web applications into mobile views, making them accessible and user-friendly across multiple platforms. My expertise extends to modern JavaScript development practices, including the implementation of ES6 features such as arrow functions, constants, block-scope variables, and class inheritance. I am well-versed in React's Virtual DOM and its unidirectional data flow, consistently delivering efficient and performant solutions. In the realm of state management, I have utilized Redux and Redux-thunk as middleware to handle asynchronous data, contributing to the creation of robust and reliable applications. My commitment to architectural excellence is evident in the incorporation of MVVM architecture in multiple projects, leveraging the power of React and Redux frameworks. Navigation is another area where I excel, having crafted various navigation systems in React Native, including Tab, Drawer, and Stack navigation, all with the goal of providing intuitive user experiences. I am adept at harnessing widgets such as TextViews, ImageViews, Buttons, ListView, ScrollView, Progress Bar, and more to create dynamic and visually appealing user interfaces.  ",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "React Native Developer",
+        where: "United Airlines",
+        date: "01/2022 - Current",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "React Native Developer",
+        where: "Starbucks",
+        date: "9/2021 - 12/2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "React Native Developer",
+        where: "Walgreens",
+        date: "7/2020 - 8/2021",
+    },
+    {
+        jobtitle: "React Native Developer",
+        where: "Rent the Runway",
+        date: "4/2019 - 6/2020",
+    },
+    {
+        jobtitle: "Frontend & Azure DevOps",
+        where: "Principle Library of Ben Souda Fes Inc",
+        date: "5/2016 - 3/2019",
     },
 ];
 
 const skills = [{
-        name: "Python",
-        value: 90,
+        name: "React Native",
+        value: 99,
     },
     {
-        name: "Djano",
+        name: "React JS",
         value: 85,
     },
     {
         name: "Javascript",
-        value: 80,
+        value: 95,
     },
     {
-        name: "React",
-        value: 60,
+        name: "Node JS",
+        value: 80,
     },
     {
         name: "Jquery",
         value: 85,
     },
+    
 ];
 
 const services = [{
@@ -74,7 +85,7 @@ const services = [{
 
 const dataportfolio = [{
         img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        description: "The0000 wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
@@ -136,8 +147,8 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
+    YOUR_EMAIL: "abdessamie.abdellah@gmail.com",
+    YOUR_FONE: "(619)613-9677",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
@@ -147,9 +158,9 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    github: "https://github.com/AbdessamieAbdellah",
+    facebook: "https://facebook.com/samie.abdel/",
+    linkedin: "https://linkedin.com/in/abdessamie-abdellah-samy-7069b5160/",
     twitter: "https://twitter.com",
 };
 export {
--- src/header/index.js
+++ src/header/index.js
@@ -56,7 +56,7 @@ const Headermain = () => {
             <a href={socialprofils.github}>Github</a>
             <a href={socialprofils.twitter}>Twitter</a>
             </div>
-            <p className="copyright m-0">copyright __ {logotext}</p>
+            <p className="copyright m-0"> Ⓒ Copyright {logotext} 2023</p>
           </div>
         </div>
       </header>
