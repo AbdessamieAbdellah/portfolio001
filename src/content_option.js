98c23f684af7f3626851bd3f2c2213852611f72a
ABDESSAMIE <abdessamie.abdellah@gmail.com>
2023-09-12 21:47:06 -0700

    data changes

--- src/content_option.js
+++ src/content_option.js
@@ -1,15 +1,15 @@
-const logotext = "JOHN";
+const logotext = "ABDESSAMIE";
 const meta = {
     title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    description: "I’m Abdessamie Abdellah react native devloper,currently working with United Airlines in USA",
 };
 
 const introdata = {
     title: "I’m John Doe",
     animated: {
         first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        second: "I build gorgeous UI/UX ",
+        third: "I develop mobile apps for IOS & Android",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
